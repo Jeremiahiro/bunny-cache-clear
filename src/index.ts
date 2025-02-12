63d8fb2ddebeb346ffc025d6945fe3adbc211996
Jeremiah Iromaka <jeremiahiro@gmail.com>
2025-02-12 20:51:17 +0000

    🎨 Clear

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import * as BunnySDK from "@bunny.net/edgescript-sdk";
 
-/**
- * Returns an HTTP response.
- * @param {Request} request - The Fetch API Request object.
- * @return {Response} The HTTP response or string.
- */
 BunnySDK.net.http.serve(async (request) => {
 
     console.log(`[INFO]: ${request.method} - ${request.url}`);
 
-return new Response(`Please provide valid zone and/or Trigger ID`);
+    return new Response(`Please provide valid zone and/or Trigger ID`);
 
 })
