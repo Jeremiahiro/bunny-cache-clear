f6cd0e955f923a0ee99c559691098ebd9bce3706
Jeremiah Iromaka <jeremiahiro@gmail.com>
2025-02-12 20:49:32 +0000

    🎨 Clear

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import * as BunnySDK from "@bunny.net/edgescript-sdk";
  */
 BunnySDK.net.http.serve(async (request) => {
 
-console.log(request)
+    console.log(`[INFO]: ${request.method} - ${request.url}`);
+
 return new Response(`Please provide valid zone and/or Trigger ID`);
 
-});
+})
